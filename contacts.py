af45037a2cef85ba44a6c6a3798500a663776ec8
JSCollegiate <113198453+SowlyBC@users.noreply.github.com>
2022-09-28 16:59:05 +0000

    Contacts now save | You can create a new name to store contacts

--- contacts.py
+++ contacts.py
@@ -10,24 +10,24 @@ from time import sleep
 # Classes
 
 class Contacts:
-  def __init__(self, key):
+  def __init__(self, key: str):
     self.uKey = key
     self.cons = db[key]
     pass
 
-  def addCon(self, conName, conNum):
+  def addCon(self, conName: str, conNum: str):
     newCon = [
       conName,
       conNum
     ]
 
     self.cons.append(newCon)
-
-    selected_con = len(self.cons.value[0])
-    print(selected_con)
-
-    print(self.cons.value[0])
+    addSpace()
+    
     return
+
+  def getCons(self):
+    return self.cons.value
 
 #
 
@@ -41,16 +41,26 @@ def init():
     newName = input('What name would you like to set? \n(you will use this name to view your saved contacts): ')
     print('Checking Availability...')
     
-    canChoose = checkDB(newName)
+    chosenName = checkDB(newName)
 
-    if canChoose:
+    if not chosenName:
       print('Name is available!')
       db[newName] = []
       print('Getting things ready...')
       setUpCon(True, newName)
-
+  
   else:
     addSpace()
+
+    keyExi = checkDB(name)
+
+    if keyExi:
+      setUpCon(False, name)
+    else:
+      print('This name doesn\'t exist!')
+      sleep(1)
+      addSpace()
+      init()
     
 
 def setUpCon(isNew: bool, key: str):
@@ -70,17 +80,44 @@ def setUpCon(isNew: bool, key: str):
         sleep(0.5)
         addSpace()
         cons.addCon(con_name, con_num)
-        
-        
+
+        loadCons(key)
+
+  else:
+    loadCons(key)
+
+def loadCons(key: str):
+  consClass = Contacts(key)
+  cons = consClass.getCons()
+
+  for c in cons:
+    print(c[0])
+    print(c[1])
+    print('\n')
+
+  print('Type \'log out\' to go back to the main screen')
+  conToAdd_name = input('Contact Name: ')
+
+  if conToAdd_name != '' and conToAdd_name != 'log out':
+    conToAdd_num = input('Contact Number: ')
+
+    if conToAdd_num != '':
+      consClass.addCon(conToAdd_name, conToAdd_num)
+
+      loadCons(key)
+
+  if conToAdd_name == 'log out':
+    addSpace()
+    init()
     
 def checkDB(key):
   keys = db.keys()
 
   for k in keys:
     if key == k:
-      return False
+      return True
 
-  return True
+  return False
 
 def addSpace():
   print('\n')
